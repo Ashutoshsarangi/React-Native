60db64a68e62b2f2c7ee2aba0b76248d4c1b990c
Ashu <ashutoshsarangi95@gmail.com>
2024-03-29 08:33:41 +0200

    Added lot of skeleton for different applications

---
+++ coffeeplayground/App.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <ScrollView>
+        <View>
+          <Text>Hello World</Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
